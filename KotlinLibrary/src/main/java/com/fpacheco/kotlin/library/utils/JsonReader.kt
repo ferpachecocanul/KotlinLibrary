8cab46bb3a532413f4c1639f17ce895e019da3cd
Fernando Pacheco <ferpachecocanul@gmail.com>
2024-12-19 13:11:05 -0600

    Se actualiza la función para que pueda ser llamado desde otra clase de menera correcta

--- KotlinLibrary/src/main/java/com/fpacheco/kotlin/library/utils/JsonReader.kt
+++ KotlinLibrary/src/main/java/com/fpacheco/kotlin/library/utils/JsonReader.kt
@@ -5,14 +5,14 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.InputStreamReader
 
-class JsonReader {
-	fun <T> readJsonFromAssetsToList(context: Context, jsonFile: String): List<T> {
+object JsonReader {
+	fun <T> readJsonFromResourceToList(context: Context, jsonFile: String): List<T> {
 		val jsonString = InputStreamReader(context.assets.open(jsonFile)).use { it.readText() }
 		val itemType = object: TypeToken<List<T>>() {}.type
 		return Gson().fromJson(jsonString, itemType)
 	}
 
-	fun <T> readJsonFromAssetsToObject(context: Context, jsonFile: String): T {
+	fun <T> readJsonFromResourceToObject(context: Context, jsonFile: String): T {
 		val jsonString = InputStreamReader(context.assets.open(jsonFile)).use { it.readText() }
 		val itemType = object: TypeToken<T>() {}.type
 		return Gson().fromJson(jsonString, itemType)
